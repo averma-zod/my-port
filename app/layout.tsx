5444cbdb4c2a936c6de1c851410fa864a1040ac5
akash.verma <avakashverma98@gmail.com>
2024-05-16 18:13:24 +0530

    site logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     siteName: "Akash-Verma.com",
     images: [
       {
-        url: "./akash-port-logo.png",
+        url: "/akash-port-logo.png",
         width: 1920,
         height: 1080,
       },
@@ -42,7 +42,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
   },
   icons: {
-    shortcut: "/favicon.png",
+    shortcut: "/akash-port-logo.png",
   },
 };
 const inter = Inter({
