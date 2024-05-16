430154286afd46ad380ab4bddd916f10ff1eef52
akash.verma <avakashverma98@gmail.com>
2024-05-16 17:23:00 +0530

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -35,14 +35,14 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
+          I'm imlementing{" "}
           <Link
             target="_blank"
-            href="https://unkey.dev"
+            href="https://digital.ai/learn/devsecops-periodic-table/"
             className="underline duration-500 hover:text-zinc-300"
           >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+            devsecops-periodic-table
+          </Link> DevSecOps solutions integrating Tools and automating the workflows securely. 
         </h2>
       </div>
     </div>
